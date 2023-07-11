2f1ce9c4be9825acc6bf4688ed89f41cd55d70d1
Brett Mason <brett@orphans.co.uk>
2023-07-11 11:01:49 +0100

    Stylelint config

---
+++ packages/stylelint-config/README.md
@@ -0,0 +1,24 @@
+# @eighteen73/stylelint-config-wordpress
+
+eighteen73 coding standards for CSS and SCSS.
+
+## Installation
+
+This package is a stylelint shareable configuration, and requires the `stylelint` library.
+
+To install this config and dependencies:
+
+```bash
+npm install --save-dev stylelint @eighteen73/stylelint-config-wordpress
+```
+
+Then, add a `.stylelintrc` file and extend these rules. You can also add your own rules and overrides for further customization.
+
+```json
+{
+  "extends": "@eighteen73/stylelint-config-wordpress",
+  "rules": {
+    ...
+  }
+}
+```
---
+++ packages/stylelint-config/index.js
@@ -0,0 +1,70 @@
+'use strict';
+
+module.exports = {
+	extends: '@wordpress/stylelint-config/scss',
+	plugins: [
+		'stylelint-order'
+  	],
+  	rules: {
+		'order/order': [
+			'custom-properties',
+			'declarations'
+		],
+		'order/properties-alphabetical-order': true,
+		'comment-empty-line-before': [
+			'always',
+			{
+				'except': [ 'first-nested' ],
+				'ignore': [ 'stylelint-commands' ]
+			}
+		],
+		'block-closing-brace-newline-before': 'always-multi-line',
+		'declaration-block-semicolon-newline-after': 'always-multi-line',
+		'declaration-block-single-line-max-declarations': 4,
+		'declaration-no-important': true,
+		'declaration-property-unit-allowed-list': {
+			'animation': [ 'ms' ],
+			'animation-delay': [ 'ms' ],
+			'animation-duration': [ 'ms' ],
+			'line-height': [],
+			'transition': [ 'ms' ],
+			'transition-delay': [ 'ms' ],
+			'transition-duration': [ 'ms' ]
+		},
+		'function-parentheses-space-inside': 'always-single-line',
+		'function-comma-space-after': 'always-single-line',
+		'function-url-quotes': 'always',
+		'max-line-length': 100,
+		'max-empty-lines': 1,
+		'max-nesting-depth': [
+			2,
+			{
+				'ignore': [ 'blockless-at-rules' ]
+			}
+		],
+		'media-feature-parentheses-space-inside': 'always',
+		'rule-empty-line-before' : [
+			'always',
+			{
+				'except': [ 'first-nested' ],
+				'ignore': [ 'after-comment' ]
+			}
+		],
+		'at-rule-empty-line-before' : [
+			'always',
+			{
+				'except': [ 'blockless-after-blockless', 'first-nested' ],
+				'ignore': [ 'after-comment' ],
+				'ignoreAtRules': [ 'if', 'else if', 'else' ]
+			}
+		],
+		'number-max-precision': 3,
+		'selector-class-pattern': [
+			'^(?<block>(?:[a-z][a-z0-9]*)(?:-[a-z0-9]+)*)(?<element>(?:__[a-z][a-z0-9]*(?:-[a-z0-9]+)*))?(?<modifier>(?:--[a-z][a-z0-9]*)(?:-[a-z0-9]+)*)?$',
+			{
+				'resolveNestedSelectors': true
+			}
+		],
+		'selector-id-pattern': null
+	}
+}
